e8005c9fedd8159459daf3d921723e9698726206
AivarasRinkevicius <71339290+AivarasRinkevicius@users.noreply.github.com>
2020-09-18 12:57:49 +0300

    Update images

--- Price-comparison-engine/Price-comparison-engine/obj/Debug/Price-comparison-engine_Content.g.i.cs
+++ Price-comparison-engine/Price-comparison-engine/obj/Debug/Price-comparison-engine_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("user.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("next.png")]
 
 
