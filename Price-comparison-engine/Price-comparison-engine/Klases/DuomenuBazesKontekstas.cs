a01306c23d579e9d92e7cf7a53f669e7170547c8
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-09-24 21:59:40 +0300

    Duombazių update

---
+++ Price-comparison-engine/Price-comparison-engine/Klases/DuomenuBazesKontekstas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Price_comparison_engine.Klases
+{
+    public class DuomenuBazesKontekstas : DbContext
+    {
+        public DbSet<DuomenuStruktura> DuomenuStrukturos { get; set; }
+    }
+}
---
+++ Price-comparison-engine/Price-comparison-engine/Klases/DuomenuStruktura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_comparison_engine.Klases
+{
+    public class DuomenuStruktura
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NaudotojoID { get; set; }
+        public string Email { get; set; }
+        public string Slaptazodis { get; set; }
+    }
+}
