6e9cce38084ca6bff68670c58f93c804928e5b6d
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-09-28 17:58:52 +0300

    Regex correction

--- Price-comparison-engine/Price-comparison-engine/RegistracijosLangas.xaml.cs
+++ Price-comparison-engine/Price-comparison-engine/RegistracijosLangas.xaml.cs
@@ -33,19 +33,23 @@ namespace Price_comparison_engine
 
         private void Registruotis_Mygtukas(object sender, RoutedEventArgs e)
         {
-            var pattern1 = new Regex(@"((\.*\d+\.*[a-zA-Z]\.*[a-zA-Z]\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*\d+\.*[a-zA-Z]\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*[a-zA-Z]\.*\d+\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*[a-zA-Z]\.*[a-zA-Z]\.*\d+\.*)", RegexOptions.Compiled);
+            var pattern1 = new Regex(@"(\.*\d+\.*[a-zA-Z]\.*[a-zA-Z]\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*\d+\.*[a-zA-Z]\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*[a-zA-Z]\.*\d+\.*[a-zA-Z]\.*)|(\.*[a-zA-Z]\.*[a-zA-Z]\.*[a-zA-Z]\.*\d+\.*)", RegexOptions.Compiled);
             var pattern2 = new Regex(@"([a-zA-Z0-9]+)(@gmail.com)$", RegexOptions.Compiled);
             if (Email.Text == "" || Slaptazodis.Password == "" || SlaptazodisPatvirtinti.Password == "")
             {
                 MessageBox.Show("Prašome užpildyti visus laukus.");
             }
+            else if (!pattern2.IsMatch(Email.Text))
+            {
+                MessageBox.Show("Email turi būti rašomas tokia tvarka:\nTuri būti naudojamos raidės arba skaičiai,\nTuri būti nors vienas skaičius arba raidė,\nEmail'o pabaiga turi baigtis: @gmail.com, pvz.: kazkas@gmail.com");
+            }
+            else if (!pattern1.IsMatch(Slaptazodis.Password))
+            {
+                MessageBox.Show("Slaptažodyje turi būti bent trys raidės ir vienas skaičius!!!");
+            }
             else if (Slaptazodis.Password != SlaptazodisPatvirtinti.Password)
             {
                 MessageBox.Show("Slaptažodžiai nesutampa.");
-            }
-            else if (!pattern2.IsMatch(Email.Text))
-            {
-                MessageBox.Show("Email turi būti rašomas tokia tvarka:\nTuri būti naudojamos raidės arba skaičiai,\nTuri būti nors vienas skaičius ir trys raidės,\nEmail'o pabaiga turi baigtis: @gmail.com, pvz.: kazkas@gmail.com");
             }
             else
             {
