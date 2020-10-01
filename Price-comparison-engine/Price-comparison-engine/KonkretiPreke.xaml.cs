350e29d469e45846008e44ec3764d9c7f2daf054
JuozasVainauskas <vainauskasjuozas@gmail.com>
2020-10-01 15:54:10 +0300

    Padarytas konkrečių prekių langas
    Pasirinkus konkrečią prekę atsiranda langas, kur bus rodomos jos kainos skirtinguose tinklapiuose

--- Price-comparison-engine/Price-comparison-engine/KonkretiPreke.xaml.cs
+++ Price-comparison-engine/Price-comparison-engine/KonkretiPreke.xaml.cs
@@ -1,14 +1,7 @@
-﻿using HtmlAgilityPack;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
-using System.Security.AccessControl;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -20,60 +13,40 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using HtmlAgilityPack;
 
 namespace Price_comparison_engine
 {
     /// <summary>
-    /// Interaction logic for PrekiųLangas.xaml
+    /// Interaction logic for KonkretiPreke.xaml
     /// </summary>
-    /// 
-
-    public partial class Item
+    partial class Item
     {
 
-        public String nuotrauka { get; set; }
-        public string Seller { get; set; }
-        public double Pricea { get; set; }
+    public String nuotraukaa { get; set; }
+    public string Sellerr { get; set; }
+    public double Priceaa { get; set; }
 
-        public string Price { get; set; }
+    public string Pricee { get; set; }
 
-        public string Name { get; set; }
+    public string Namee { get; set; }
 
-        public string Link { get; set; }
+    public string Linkk { get; set; }
 
     }
-
-    public partial class PrekiuLangas : Window
-    {
-        public PrekiuLangas()
+public partial class KonkretiPreke : Window
+{
+    public static string pav;
+        public KonkretiPreke(string pavadinimas)
         {
+            pav = pavadinimas;
+            Console.WriteLine(pav);
             InitializeComponent();
         }
 
         private static async void getHtmlAssync(DataGrid dataGridas)
         {
             var prices = new List<Item>();
-            /*
-            var chromeOptions = new ChromeOptions();
-            //var user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
-            //chromeOptions.AddArgument("f'user-agent="+user_agent);
-            //chromeOptions.AddArgument("headless");
-            //chromeOptions.AddArgument("window-size=1920,1080");
-            //chromeOptions.AddArgument("log-level=3");
-            IWebDriver driver = new ChromeDriver(chromeOptions);
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://www.topocentras.lt/");
-            var element = driver.FindElement(By.XPath("/html/body/div[1]/header[1]/div[1]/div[2]/div[1]/div[1]/input"));
-            element.SendKeys(MainWindow.zodis);
-            driver.FindElement(By.XPath("/html/body/div[1]/header[1]/div[1]/div[2]/div[1]/div[1]/button")).Click();
-            var prekes = driver.FindElements(By.ClassName("ProductGrid-productName-1JN"));
-            foreach(var preke in prekes)
-            {
-                var Itemas = new Item { Seller = "Topo  Centras", Name = preke.Text};
-                prices.Add(Itemas);
-            }
-            */
             var piguDaiktai = piguPaieska(await piguHtmlPaemimas());
             var avitelosDaiktai = avitelosPaieska(await avitelosHtmlPaemimas());
             var elektromarktDaiktai = elektromarktPaieska(await elektromarktHtmlPaemimas());
@@ -86,7 +59,7 @@ namespace Price_comparison_engine
 
         private static async Task<HtmlDocument> avitelosHtmlPaemimas()
         {
-            var url = "https://avitela.lt/paieska/" + MainWindow.zodis;
+            var url = "https://avitela.lt/paieska/" + pav;
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(url);
             var htmlDocument = new HtmlDocument();
@@ -97,7 +70,7 @@ namespace Price_comparison_engine
         private static async Task<HtmlDocument> elektromarktHtmlPaemimas()
         {
             Regex regEx = new Regex(" ");
-            var urlgalas = regEx.Replace(MainWindow.zodis, "+");
+            var urlgalas = regEx.Replace(pav, "+");
             var url2 = "https://www.elektromarkt.lt/lt/catalogsearch/result/?order=price&dir=desc&q=" + urlgalas;
             var httpClient2 = new HttpClient();
             var html2 = await httpClient2.GetStringAsync(url2);
@@ -111,7 +84,7 @@ namespace Price_comparison_engine
             try
             {
                 Regex regEx = new Regex(" ");
-                var urlgalas = regEx.Replace(MainWindow.zodis, "+");
+                var urlgalas = regEx.Replace(pav, "+");
                 var url2 = "https://pigu.lt/lt/search?q=" + urlgalas;
                 var httpClient2 = new HttpClient();
                 var html2 = await httpClient2.GetStringAsync(url2);
@@ -213,7 +186,7 @@ namespace Price_comparison_engine
                         var priceAtsarg = price;
                         priceAtsarg = pasalinimasEuroSimbol(priceAtsarg);
                         double pricea = Convert.ToDouble(priceAtsarg);
-                        var Itemas = new Item { Seller = "Avitela", Name = name, Pricea = pricea, Price = price, Link = link };
+                        var Itemas = new Item { Sellerr = "Avitela", Namee = name, Priceaa = pricea, Pricee = price, Linkk = link };
                         prices.Add(Itemas);
                     }
                 }
@@ -253,9 +226,8 @@ namespace Price_comparison_engine
                     priceAtsarg = pasalinimasEuroSimbol(priceAtsarg);
 
                     double pricea = Convert.ToDouble(priceAtsarg);
-                    var Itemas = new Item { nuotrauka = imgLink, Seller = "Pigu", Name = name, Pricea = pricea, Price = price, Link = link };
+                    var Itemas = new Item { nuotraukaa = imgLink, Sellerr = "Pigu", Namee = name, Priceaa = pricea, Pricee = price, Linkk = link };
                     prices.Add(Itemas);
-                    RasytiData(link, imgLink);
                 }
             }
             else
@@ -288,9 +260,8 @@ namespace Price_comparison_engine
                     priceAtsarg = pasalinimasEuroSimbol(priceAtsarg);
 
                     double pricea = Double.Parse(priceAtsarg);
-                    var Itemas = new Item { nuotrauka = imgLink, Seller = "Elektromarkt", Name = name, Pricea = pricea, Price = price, Link = link };
+                    var Itemas = new Item { nuotraukaa = imgLink, Sellerr = "Elektromarkt", Namee = name, Priceaa = pricea, Pricee = price, Linkk = link };
                     prices.Add(Itemas);
-                    RasytiData(link, imgLink);
 
                 }
             }
@@ -357,100 +328,25 @@ namespace Price_comparison_engine
             prices.Clear();
         }
 
-
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            RectangleIstempimasAukstis(rectangle1);
-            RectangleIstempimasPlotis(rectangle2);
-        }
-
-        private void RectangleIstempimasAukstis(Rectangle plotelis)
-        {
-            plotelis.Height = this.ActualHeight;
-        }
-
-        private void RectangleIstempimasPlotis(Rectangle plotelis)
-        {
-            plotelis.Width = this.ActualWidth;
-        }
         private void DataGridTest_Initialized(object sender, EventArgs e)
         {
-            getHtmlAssync(DataGridas);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            VertinimoLangas vertinimoLangoAtidarymas = new VertinimoLangas();
-            vertinimoLangoAtidarymas.Show();
-        }
-
-        private void linkButton_Click(object sender, RoutedEventArgs e)
-        {
-            string link = (((Button)sender).DataContext as Item).Link;
-            if (link != null)
-            {
-                System.Diagnostics.Process.Start(link);
-            }
-        }
-        private static void RasytiData(string puslapioURL, string imgURL)
-        {
-            var sqlPrisijungti = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\PCEDatabase.mdf;Integrated Security=SSPI;Connect Timeout=30");
-            try
-            {
-                if (sqlPrisijungti.State == ConnectionState.Closed)
-                {
-                    sqlPrisijungti.Open();
-                }
-
-                var duomenuAdapteris = new SqlDataAdapter("SELECT PuslapioURL, ImgURL FROM PuslapiuDuomenys WHERE PuslapioURL ='" + puslapioURL + "' AND ImgURL ='" + imgURL + "' ", sqlPrisijungti);
-                var duomenuLentele = new DataTable();
-                duomenuAdapteris.Fill(duomenuLentele);
-                if (duomenuLentele.Rows.Count == 0)
-                {
-                    try
-                    {
-                        if (sqlPrisijungti.State == ConnectionState.Closed)
-                        {
-                            sqlPrisijungti.Open();
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-
-                    var eile = "INSERT INTO PuslapiuDuomenys(PuslapioURL,ImgURL) VALUES (@PuslapioURL, @ImgURL)";
-                    var sqlKomanda = new SqlCommand(eile, sqlPrisijungti);
-                    sqlKomanda.CommandType = CommandType.Text;
-                    sqlKomanda.Parameters.AddWithValue("@PuslapioURL", puslapioURL);
-                    sqlKomanda.Parameters.AddWithValue("@ImgURL", imgURL);
-                    sqlKomanda.ExecuteNonQuery();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                sqlPrisijungti.Close();
-            }
+            getHtmlAssync(DataGridass);
         }
 
         private void imageClick_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            /*
-            string link = (((Image)sender).DataContext as Item).Link;
-            if (link != null)
-            {
-                System.Diagnostics.Process.Start(link);
-            }
-            */
-            string pavadinimas= (((Image)sender).DataContext as Item).Name;
-            KonkretiPreke langas = new KonkretiPreke(pavadinimas);
-            langas.Show();
+            throw new NotImplementedException();
+        }
+
+        private void linkButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void konkretiPrekeLangas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DataGridass.Width = this.ActualWidth;
+            DataGridass.Width = this.ActualHeight;
         }
     }
 }
-
-
