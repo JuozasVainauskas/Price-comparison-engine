8cc33494be9ee25a5db0b3d29fb5d32adfb533b3
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-10-03 13:40:48 +0300

    Duomenu bazės update
    Pridėti laukeliai, NaudotojoDuomenys, lentelėje - ArVertino, Role

--- Price-comparison-engine/Price-comparison-engine/Klases/NaudotojoDuomenys.cs
+++ Price-comparison-engine/Price-comparison-engine/Klases/NaudotojoDuomenys.cs
@@ -16,5 +16,7 @@ namespace Price_comparison_engine.Klases
         public string Email { get; set; }
         public string SlaptazodzioHash { get; set; }
         public string SlaptazodzioSalt { get; set; }
+        public int ArBalsavo { get; set; }
+        public int Role { get; set; }
     }
 }
