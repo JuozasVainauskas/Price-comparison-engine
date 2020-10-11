7dd4841995a5cedfa73e750deff3f8a228cf1835
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-10-11 19:31:00 +0300

    Update v1

---
+++ Price-comparison-engine/Price-comparison-engine/Klases/SavedItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_comparison_engine.Klases
+{
+    public class SavedItems
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SavedItemID { get; set; }
+        public string Email { get; set; }
+        public string PageURL { get; set; }
+        public string ImgURL { get; set; }
+        public string ShopName { get; set; }
+        public string ItemName { get; set; }
+        public string Price { get; set; }
+    }
+}
