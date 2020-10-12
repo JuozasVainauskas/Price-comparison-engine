9e4a2bef7f7a1f2cb35f8f64758b6da0d6999154
AivarasRinkevicius <71339290+AivarasRinkevicius@users.noreply.github.com>
2020-10-12 13:17:58 +0300

    update v2

--- Price-comparison-engine/Price-comparison-engine/MainWindowLoggedIn.xaml.cs
+++ Price-comparison-engine/Price-comparison-engine/MainWindowLoggedIn.xaml.cs
@@ -53,24 +53,6 @@ namespace Price_comparison_engine
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            /* var skirtumasPlocio = this.ActualWidth / 1.2;
-             var skirtumasIlgio = this.ActualHeight / 1.1;
-             var skirtumasPlocioBlokeliui = this.ActualHeight / 1.05;
-             var skirtumasPlocioNuotraukai = this.ActualWidth / 1.2;
-             var skirtumasIlgioNuotraukai = this.ActualHeight / 1.2;
-             MygtukoResize(Ieškoti, skirtumasPlocio, skirtumasIlgio);
-             TextBoxResize(ieškojimoLaukas, skirtumasPlocio / 3, skirtumasIlgio);
-             RectangleIštempimas(viršutinėlinija);
-             RectangleIštempimas(vidurinėLinija);
-             //RectangleResize(vidurinėLinija, skirtumasPlocioBlokeliui);
-             RectangleIštempimas(vidurinėLinija2);
-             //RectangleResize(vidurinėLinija2, skirtumasPlocioBlokeliui);
-             RectangleIštempimas(apatinėLinija);
-             SlideShowResize(img1_2, skirtumasPlocioNuotraukai, skirtumasIlgioNuotraukai);
-             SlideShowResize(img2_2, skirtumasPlocioNuotraukai, skirtumasIlgioNuotraukai);
-             SlideShowResize(img3_2, skirtumasPlocioNuotraukai, skirtumasIlgioNuotraukai);
-             SlideShowResize(iKairePuse1, skirtumasIlgio, skirtumasIlgio);
-             SlideShowResize(iDesinePuse1, skirtumasIlgio, skirtumasIlgio);*/
             var skirtumasPlocioNuotraukai = this.ActualWidth / 1.1;
             var skirtumasIlgioNuotraukai = this.ActualHeight / 1.1;
             var skirtumasPlocio = this.ActualWidth / 1.2;
